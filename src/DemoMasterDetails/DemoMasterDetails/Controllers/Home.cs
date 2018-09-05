8daa811c59feb52f6fa9147a2d01c06067221dd5
Fabiano Nalin <fabiano.nalin@gmail.com>
2018-09-05 11:50:07 -0300

    Subindo o projeto

---
+++ src/DemoMasterDetails/DemoMasterDetails/Controllers/Home.cs
@@ -0,0 +1,25 @@
+﻿using DemoMasterDetails.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMasterDetails.Controllers
+{
+    public class Home:Controller
+    {
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void Salvar(Cliente cliente)
+        {
+
+        }
+    }
+}
---
+++ src/DemoMasterDetails/DemoMasterDetails/Models/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoMasterDetails.Models
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+        public IList<Telefone> Telefones { get; set; } = new List<Telefone>();
+
+    }
+
+
+    public class Telefone
+    {
+        public int DDD { get; set; }
+        public string Numero { get; set; }
+    }
+}
